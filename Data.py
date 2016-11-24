16ee87a859469a19e6e1f71c5506f12fed2f2b3d
Dlugosz Tristan <tristand.pro@gmail.com>
2016-11-24 20:20:56 +0100

    Modification a Data.py for use a dict with use newfile()

--- Data.py
+++ Data.py
@@ -1,2 +1,6 @@
 pathpy = "#!/usr/bin/python3.4"
 encoding = "# -*-coding:utf-8 -*"
+extension = {"py": "#!/usr/bin/python3.4\n# -*-coding:utf-8 -*\n",
+             "html": "<DOCTYPE html>\n<head>\n</head>\n<body>\n</body>",
+             "xml": "",
+             "setup": ""}
--- idepy.py
+++ idepy.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3.4
 # -*-coding:utf-8 -*
+
 import sys
 import Data
 
 
-def newfile():
-    for arg in sys.argv[2:]:
-        x = arg + ".py"
-        file = open(x, "w")
-        file.writelines(Data.pathpy + "\n")
-        file.writelines(Data.encoding + "\n")
+def newfile(x, n):
+    for arg in sys.argv[n:]:
+        y = arg + "." + x
+        file = open(y, "w")
+        file.writelines(Data.extension[x])
         file.close
         print("new file created {0}".format(arg))
 
 
 if __name__ == "__main__":
-    print(type(sys.argv))
-    print(sys.argv[1])
+
     if sys.argv[1] == "new":
-        newfile()
+        if Data.extension[sys.argv[2]]:
+            newfile(sys.argv[2], 3)
+        else:
+            newfile("py", 2)
+
