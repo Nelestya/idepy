5f5ec0bf442360dd7bb58136d4147172aed98d04
Dlugosz Tristan <tristand.pro@gmail.com>
2016-11-25 03:14:35 +0100

    error management for create a file

--- Data.py
+++ Data.py
@@ -1,5 +1,4 @@
-pathpy = "#!/usr/bin/python3.4"
-encoding = "# -*-coding:utf-8 -*"
+
 extension = {"py": "#!/usr/bin/python3.4\n# -*-coding:utf-8 -*\n",
              "html": "<DOCTYPE html>\n<head>\n</head>\n<body>\n</body>",
              "xml": "",
--- idepy.py
+++ idepy.py
@@ -7,18 +7,27 @@ import Data
 
 def newfile(x, n):
     for arg in sys.argv[n:]:
-        y = arg + "." + x
-        file = open(y, "w")
+        filename = arg + "." + x
+        file = open(filename, "w")
         file.writelines(Data.extension[x])
         file.close
         print("new file created {0}".format(arg))
+    """create a new file with extension in Data"""
+
+
+def newfileManager():
+    try:
+        if sys.argv[1] == "new":
+            try:
+                if Data.extension[sys.argv[2]]:
+                    newfile(sys.argv[2], 3)
+            except KeyError:
+                newfile("py", 2)
+    except IndexError:
+        quit()
+    """condition for create a new file, use arguments new"""
+    """if a specify file use the dictionnary in the Data.extension"""
 
 
 if __name__ == "__main__":
-
-    if sys.argv[1] == "new":
-        if Data.extension[sys.argv[2]]:
-            newfile(sys.argv[2], 3)
-        else:
-            newfile("py", 2)
-
+    newfileManager()
